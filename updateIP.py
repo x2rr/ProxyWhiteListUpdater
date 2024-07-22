ea0f030d4b0e92c575877de0a26a26a75499b9f3
Liu Rabt <136670930@qq.com>
2024-07-22 10:58:25 +0800

    Update updateIP.py
    增加优亦云，增加青龙环境变量

--- updateIP.py
+++ updateIP.py
@@ -4,14 +4,41 @@
 import requests
 import hashlib
 import urllib.parse
+import os
 
 
-JULIANG_KEY = ''  # 填入巨量的 Key
-JULIANG_TRADE_NO = ''  # 填入巨量的 Trade No
-XK_APIKEY = ''  # 填入星空的 API Key
-XK_SIGN = ''  # 填入星空的 Sign
-XIEQU_UID = ''  # 填入携趣的 UID
-XIEQU_UKEY = ''  # 填入携趣的 UKEY
+# JULIANG_KEY = ''  # 填入巨量的 Key
+# JULIANG_TRADE_NO = ''  # 填入巨量的 Trade No
+# XK_APIKEY = ''  # 填入星空的 API Key
+# XK_SIGN = ''  # 填入星空的 Sign
+# XIEQU_UID = ''  # 填入携趣的 UID
+# XIEQU_UKEY = ''  # 填入携趣的 UKEY
+# YYY_UID = ''  # 填入优亦云的用户套餐ID
+# YYY_TOKEN = ''  # 填入优亦云的TOKEN
+# 巨量
+JULIANG_KEY = ''
+JULIANG_TRADE_NO = '' 
+# 星空
+XK_APIKEY = ''
+XK_SIGN = ''
+# 携趣
+XIEQU_UID = ''
+XIEQU_UKEY = ''
+# 优亦云
+YYY_UID = ''
+YYY_TOKEN = ''
+
+# 青龙环境变量（若上面不填写，则读取青龙环境变量）
+JULIANG_KEY = JULIANG_KEY if JULIANG_KEY else os.getenv("JULIANG_KEY")
+JULIANG_TRADE_NO = JULIANG_TRADE_NO if JULIANG_TRADE_NO else os.getenv("JULIANG_TRADE_NO")
+XK_APIKEY = XK_APIKEY if XK_APIKEY else os.getenv("XK_APIKEY")
+XK_SIGN = XK_SIGN if XK_SIGN else os.getenv("XK_SIGN")
+XIEQU_UID = XIEQU_UID if XIEQU_UID else os.getenv("XIEQU_UID")
+XIEQU_UKEY = XIEQU_UKEY if XIEQU_UKEY else os.getenv("XIEQU_UKEY")
+YYY_UID = YYY_UID if YYY_UID else os.getenv("YYY_UID")
+YYY_TOKEN = YYY_TOKEN if YYY_TOKEN else os.getenv("YYY_TOKEN")
+
+
 
 
 class SignKit:
@@ -66,6 +93,21 @@ def update_xiequ_white_list(ip, XIEQU_UID, XIEQU_UKEY):
         else:
             return '携趣白名单ip未变化'
 
+def update_yyy_white_list(ip, YYY_UID, YYY_TOKEN):
+    if YYY_UID and YYY_TOKEN:
+        url = f'http://data.yyyip.cn:88/whiteip_api?method=list&token={YYY_TOKEN}'
+        response = requests.get(url)
+        data = response.json()
+        arr = [d["ip"] for d in data['data']]
+        ipstr = ','.join(map(str, arr))
+        if ip not in arr:
+            requests.get(f'http://data.yyyip.cn:88/whiteip_api?method=del&token={YYY_TOKEN}&ip={ipstr}')
+            response = requests.get(f'http://data.yyyip.cn:88/whiteip_api?method=add&token={YYY_TOKEN}&upackid={YYY_UID}&ip={ip}')
+            return response.json()['msg']
+            # return '更新优亦云白名单成功' if response.status_code == 200 else '更新优亦云白名单出错'
+        else:
+            return '优亦云白名单ip未变化'
+
 
 def main():
     ip = get_current_ip()
@@ -74,6 +116,7 @@ def main():
     print('更新巨量白名单结果：', update_juliang_white_list(ip, JULIANG_KEY, JULIANG_TRADE_NO))
     print('更新星空白名单结果：', update_xk_white_list(ip, XK_APIKEY, XK_SIGN))
     print('更新携趣白名单结果：', update_xiequ_white_list(ip, XIEQU_UID, XIEQU_UKEY))
+    print('更新优亦云白名单结果：', update_yyy_white_list(ip, YYY_UID, YYY_TOKEN))
 
 if __name__ == "__main__":
     main()
